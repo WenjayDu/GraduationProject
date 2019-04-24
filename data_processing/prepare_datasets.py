0d4d738e18510e8d1b125502e019b76698c58f02
WenjayDu <wenjay.du@gmail.com>
2019-04-24 11:21:30 +0800

    refactor(prepare_datasets): adjust code;

--- data_processing/prepare_datasets.py
+++ data_processing/prepare_datasets.py
@@ -8,21 +8,32 @@ def prepare_mri_dataset():
     """
     used to prepare mri dataset with padding = 4
     """
-    current_dir_pardir = config.get_pardir_containing_file(__file__)
-    print("uncompressing the output.tar.bz2 file from minc_keras project...")
-    os.system("tar -jxvf " + current_dir_pardir + "/module_minc_keras/data/output.tar.bz2  &> /dev/null")
-    os.system("mv ./output " + current_dir_pardir + "/datasets/mri")
-    print("uncompressing done")
+    project_path = config.get_project_path()
+    os.chdir(project_path)
+    if not os.path.exists(project_path + "/datasets/mri"):
+        print("decompressing the output.tar.bz2 file from minc_keras project...")
 
-    setup_dirs(current_dir_pardir + '/datasets/mri_pad_4_results')
-    os.chdir(current_dir_pardir + "/datasets")
+        cmd = "tar -jxvf " + project_path + "/module_minc_keras/data/output.tar.bz2 -C " + project_path + "/datasets &> /dev/null"
+        print("executing -> ", cmd)
+        os.system(cmd)
+
+        cmd = "mv " + project_path + "/datasets/output " + project_path + "/datasets/mri"
+        print("executing -> ", cmd)
+        os.system(cmd)
+
+        print("done decompressing")
+
+    setup_dirs(project_path + '/datasets/mri_pad_4_results')
+
+    os.chdir(project_path + "/datasets")
     [images_mri_pad_4, data_mri_pad_4] = prepare_data('mri', 'mri_pad_4_results/data', 'mri_pad_4_results/report',
                                                       input_str='_T1w_anat_rsl.mnc',
                                                       label_str='variant-seg',
                                                       pad_base=4,
                                                       clobber=True)
+    os.chdir(project_path)
     print("\nmri dataset prepared\n")
-    os.chdir(config.get_project_path())
+
     return [images_mri_pad_4, data_mri_pad_4]
 
 
