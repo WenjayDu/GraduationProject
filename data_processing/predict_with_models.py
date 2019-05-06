f45f58a3d1715ab3c746815c98be98fa8373c53b
WenjayDu <wenjay.du@gmail.com>
2019-05-06 19:40:41 +0800

    feat(data_processing/predict_with_models): enable converting all png images in a fold directly;

--- data_processing/predict_with_models.py
+++ data_processing/predict_with_models.py
@@ -6,7 +6,7 @@ import numpy as np
 import tensorflow as tf
 from keras.preprocessing import image
 
-from config_and_utils import GlobalVar
+from config_and_utils import GlobalVar, get_sorted_files
 from module_minc_keras.utils import normalize
 from unet_constructing.construct_unet_with_tf import UNet
 
@@ -27,7 +27,7 @@ TF_PREDICTION_SAVED_DIR = OUTPUT_PATH + "/tf_model"
 PREDICT_BATCH_SIZE = 1
 
 
-def to_hot_cmap(img, save_path, argmax_axis=2):
+def show_argmax_result(img, argmax_axis=2):
     """
     convert an img to a hot colormap
     :param img: path of an img to be converted or an numpy array
@@ -37,15 +37,67 @@ def to_hot_cmap(img, save_path, argmax_axis=2):
     """
     if type(img) == str:
         img = image.load_img(img)
+        img.show()
         img = image.img_to_array(img)
+    print("image.shape", img.shape)
     img = np.argmax(img, axis=argmax_axis)
-    if len(list(img.shape)) == 3:
+    img = np.expand_dims(img, axis=argmax_axis)
+    img = image.array_to_img(img)
+    img.show()
+
+
+def to_hot_cmap(img, save_path=None, argmax_axis=2):
+    """
+    convert an img to a hot colormap
+    :param img: path of an img or a dir containing png images to be converted or an numpy array
+    :param save_path: save path
+    :param argmax_axis:
+    :return:
+    """
+    if type(img) == str:
+        if os.path.isfile(img):
+            img = image.load_img(img)
+            img = image.img_to_array(img)
+        elif os.path.isdir(img):
+            print("🚩Converting all .png files in", img)
+            image_list = get_sorted_files(img, "png")
+            example = image.load_img(image_list[0])
+            example = image.img_to_array(example)
+            fig = plt.figure(frameon=False,
+                             figsize=(example.shape[1] / 500, example.shape[0] / 500),  # figsize(width, height)
+                             dpi=500)
+            ax = plt.Axes(fig, [0., 0., 1., 1.])
+            ax.set_axis_off()
+            fig.add_axes(ax)
+            if not os.path.exists(img + "/to_hot_cmap"):
+                os.makedirs(img + "/to_hot_cmap")
+            for i in image_list:
+                save_path = img + "/to_hot_cmap/" + i.split('/')[-1]
+                img_arr = image.load_img(i)
+                img_arr = image.img_to_array(img_arr)
+                img_arr = np.argmax(img_arr, axis=argmax_axis)
+                img_arr = normalize(img_arr)
+                ax.imshow(img_arr, cmap="hot")
+                fig.savefig(save_path)
+                ax.clear()
+            print("🚩Done converting, converted images are saved to", img + "/to_hot_cmap")
+            plt.close()
+            return 0
+    img = np.argmax(img, axis=argmax_axis)
+    if len(list(img.shape)) == 3:  # this means the img is a tensor, in which the 1st dim is the num of samples
         img = img.reshape(list(img.shape[1:3]))
     img = normalize(img)
-    plt.imshow(img, cmap="hot")
-    plt.axis('off')
-    plt.savefig(save_path)
+
+    fig = plt.figure(frameon=False,
+                     figsize=(img.shape[1] / 500, img.shape[0] / 500),  # figsize(width, height)
+                     dpi=500)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    ax.imshow(img, cmap="hot")
+    fig.savefig(save_path)
     print("🚩Saved prediction to", save_path)
+    plt.close()
     del img
 
 
