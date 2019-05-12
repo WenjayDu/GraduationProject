1f86e82f41774c032aee8b16d92f71487c3962f0
WenjayDu <wenjay.du@gmail.com>
2019-05-12 15:14:03 +0800

    feat(preapre_datasets, convert_npy_to_tfrecords): enable setting the saving path for generated datasets;

--- data_processing/prepare_datasets.py
+++ data_processing/prepare_datasets.py
@@ -6,7 +6,7 @@ curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
-from config_and_utils import GlobalVar
+from config_and_utils import GlobalVar, logging
 from module_minc_keras.minc_keras import setup_dirs
 from module_minc_keras.prepare_data import prepare_data
 
@@ -19,11 +19,12 @@ def serialize_object(data, file_path):
         pickle.dump(data, f)
 
 
-def prepare_dataset(dataset_name="mri", ratios=[0.75, 0.15]):
+def prepare_dataset(dataset_name="mri", ratios=[0.75, 0.15], save_path=None):
     """
     used to prepare dataset with padding = 4
     :param dataset_name: name of dataset, "mri" or "sorteo", default "mri"
     :param ratios: a list containing 2 elements, the ratio of train and validation dataset respectively
+    :param save_path: save path of results, or just a dir name, which will be created under DATASET_PATH
     :return:
     """
     if not (dataset_name == "mri" or dataset_name == "sorteo"):
@@ -31,35 +32,41 @@ def prepare_dataset(dataset_name="mri", ratios=[0.75, 0.15]):
     dataset_name = "/" + dataset_name
     os.chdir(DATASET_PATH)
     if not os.path.exists(DATASET_PATH + dataset_name):
-        print("❗️" + dataset_name + " source data does not exist, start creating...")
+        logging.warning("❗️" + dataset_name + " source data does not exist, start creating...")
         try:
             if dataset_name == "/mri":
-                print("🚩decompressing the output.tar.bz2 file from minc_keras project...")
+                logging.info("🚩decompressing the output.tar.bz2 file from minc_keras project...")
                 cmd = "tar -jxvf " + PROJECT_PATH + "/module_minc_keras/data/output.tar.bz2 -C " + PROJECT_PATH + "/datasets &> /dev/null"
-                print("🚩executing -> ", cmd)
+                logging.info("🚩executing -> " + cmd)
                 os.system(cmd)
                 cmd = "mv " + DATASET_PATH + "/output " + DATASET_PATH + "/mri"
-                print("🚩executing -> ", cmd)
+                logging.info("🚩executing -> " + cmd)
                 os.system(cmd)
             elif dataset_name == "/sorteo":
-                print("🚩downloading sorteo dataset...")
+                logging.info("🚩downloading sorteo dataset...")
                 cmd = "wget https://amnesia.cbrain.mcgill.ca/deeplearning/sorteo.tar.bz2 --no-check-certificate"
-                print("🚩executing -> ", cmd)
+                logging.info("🚩executing -> " + cmd)
                 os.system(cmd)
                 cmd = "mkdir sorteo && tar -jxvf sorteo.tar.bz2 -C sorteo &> /dev/null"
-                print("🚩executing -> ", cmd)
+                logging.info("🚩executing -> " + cmd)
                 os.system(cmd)
             print("done creating")
         except Exception:
             exit("❗️some error happened, please try again")
+
     # make dirs to store generated dataset
-    path_to_store_result = DATASET_PATH + dataset_name + '_pad_4_results'
-    setup_dirs(path_to_store_result)
+    if save_path is None:
+        save_path = DATASET_PATH + dataset_name + '_pad_4'
+    else:
+        if save_path.__contains__("/") is False:
+            save_path = DATASET_PATH + "/" + save_path
+    logging.info("🚩Generated datasets will be saved to " + save_path + ", now setting up dirs...")
+    setup_dirs(save_path)
 
     source_data_dir = DATASET_PATH + dataset_name
-    data_dir = path_to_store_result + "/data"
-    report_dir = path_to_store_result + "/report"
-    serialized_file = path_to_store_result + "/serialized_dataset_object"
+    data_dir = save_path + "/data"
+    report_dir = save_path + "/report"
+    serialized_file = save_path + "/serialized_dataset_object"
     input_str, label_str = None, None
     if dataset_name == "/mri":
         input_str = '_T1w_anat_rsl.mnc'
@@ -75,8 +82,8 @@ def prepare_dataset(dataset_name="mri", ratios=[0.75, 0.15]):
                           ratios=ratios)
     os.chdir(PROJECT_PATH)
     serialize_object(result, serialized_file)
-    print("🚩returned object of prepare_data() has been writen to", serialized_file)
-    print("🚩Done preparing " + path_to_store_result + " dataset\n")
+    logging.info("🚩returned object of prepare_data() has been writen to " + serialized_file)
+    logging.info("🚩Done preparing " + save_path + " dataset\n")
 
 
 if __name__ == "__main__":
